53af579a6fd2a06033232ccb3ca39d54f4b1e023
Ojas Anand <oanand@fastmail.com>
2021-03-25 17:47:11 -0400

    Fixed texture application and slightly improved zoom functionality

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from moderngl_window import WindowConfig
 
 from logging import getLogger
 from colour import Color
-from os.path import normpath, join
 import numpy as np
 
 logger = getLogger(__name__)
@@ -12,6 +11,9 @@ w, h = 1280, 720
 a_ratio = 16 / 9
 
 color_palette = [Color("black"), Color("blue"), Color("red")]
+color_palette_len = len(color_palette)
+
+scale_highest_zoom = 0.0001
 
 mand_vtx_shader = '''
 #version 330
@@ -66,6 +68,16 @@ void main()
 '''
 
 
+def normalize_xy(x, y, x_offset):
+    norm_x = 1.0 - 2.0 * float(x / w)
+    norm_y = -1.0 + 2.0 * float(y / h)
+
+    norm_x += x_offset
+    norm_x = norm_x - 1 if norm_x > 1 else norm_x
+
+    return norm_x, norm_y
+
+
 class MainWindow(WindowConfig):
     gl_version = (3, 3)
     window_size = (w, h)
@@ -86,18 +98,25 @@ class MainWindow(WindowConfig):
         self.max_iters = self.prog['Max_Iters']
 
         start_inx = 0
-        sections = int(256 / (len(color_palette) - 1))
+        sections = int(256 / color_palette_len)
         end_inx = sections
+
         color_bytes = np.zeros((256, 1, 3), dtype=np.uint8)
+
+        if color_palette_len % 2 != 0:
+            start_inx += 1
+            end_inx += 1
+            color_bytes[0, 0] = np.array([0, 0, 0]).astype(np.uint8)
+
         for i in range(len(color_palette) - 1):
             section_gradient = list(color_palette[i].range_to(color_palette[i + 1], sections))
             color_bytes[start_inx: end_inx, 0] = [np.array(np.array(list(color.get_rgb())) * 255).astype(np.uint8)
                                                    for color in section_gradient]
-            start_inx = sections
+            start_inx = end_inx
             end_inx += sections
 
-        print(color_bytes)
-        self.mouse_center = (0.5, 0.0)
+        self.x_vid_offset = 0.5
+        self.mouse_center = (self.x_vid_offset, 0.0)
         self.center.value = self.mouse_center
         self.scale.value = 1.5
         self.max_iters.value = 100
@@ -118,33 +137,52 @@ class MainWindow(WindowConfig):
 
     def mouse_scroll_event(self, x_offset: float, y_offset: float):
         if y_offset > 0:
-            if self.scale.value > 0.1:
-                self.center.value = normalize_xy(self.mouse_center[0], self.mouse_center[1])
-                self.scale.value -= 0.1
-            elif self.scale.value > 0.01:
-                self.center.value = normalize_xy(self.mouse_center[0], self.mouse_center[1])
-                self.scale.value -= 0.01
-            elif self.scale.value > 0.001:
-                self.center.value = normalize_xy(self.mouse_center[0], self.mouse_center[1])
-                self.scale.value -= 0.001
-        elif y_offset < 0 and self.scale.value < 1.5:
+            if self.scale.value > scale_highest_zoom:
+                self.center.value = normalize_xy(self.mouse_center[0] , self.mouse_center[1], self.x_vid_offset)
+
+                if self.scale.value >= 0.1:
+                    self.scale.value -= 0.1
+                elif self.scale.value >= 0.01:
+                    self.scale.value -= 0.01
+                elif self.scale.value >= 0.001:
+                    self.scale.value -= 0.001
+                else:
+                    self.scale.value -= scale_highest_zoom
+        elif y_offset < 0:
             if self.scale.value < 1.5:
-                self.center.value = normalize_xy(self.mouse_center[0], self.mouse_center[1])
-                self.scale.value += 0.1
+                self.center.value = normalize_xy(self.mouse_center[0], self.mouse_center[1], self.x_vid_offset)
+
+                if self.scale.value <= 0.001:
+                    self.scale.value += scale_highest_zoom
+                elif self.scale.value <= 0.01:
+                    self.scale.value += 0.001
+                elif self.scale.value <= 0.1:
+                    self.scale.value += 0.01
+                else:
+                    self.scale.value += 0.1
             else:
-                self.center.value = (0.5, 0.0)
                 self.scale.value = 1.5
+                self.center.value = (0.5, 0.0)
+
+        # print("Zoom")
 
     def mouse_position_event(self, x, y, dx, dy):
         self.mouse_center = (x, y)
-
-
-def normalize_xy(x, y):
-    norm_x = 1.0 - 2.0 * float(x / w);
-    norm_y = -1.0 + 2.0 * float(y / h);
-
-    return norm_x, norm_y
+        # print("Mouse Position Captured")
 
 
 if __name__ == "__main__":
     MainWindow.run()
+    # start_inx = 0
+    # sections = int(256 / (len(color_palette) - 1))
+    # end_inx = sections
+    # color_bytes = np.zeros((256, 1, 3), dtype=np.uint8)
+    # for i in range(len(color_palette) - 1):
+    #     section_gradient = list(color_palette[i].range_to(color_palette[i + 1], sections))
+    #     color_bytes[start_inx: end_inx, 0] = [np.array(np.array(list(color.get_rgb())) * 255).astype(np.uint8)
+    #                                           for color in section_gradient]
+
+    # print(color_bytes)
+    # from PIL import Image
+    # test = Image.fromarray(color_bytes, "RGB")
+    # test.show()
